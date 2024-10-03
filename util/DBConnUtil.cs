689adf84df22c9df47755b7c934cfe5581f5d6f2
Tarun <86006288+Taruns25@users.noreply.github.com>
2024-10-03 16:42:10 +0530

    Add files via upload

---
+++ util/DBConnUtil.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace projectmanagement.util
+{
+    public class DBConnUtil
+    {
+        private static IConfigurationRoot Configuration { get; set; }
+
+        static DBConnUtil()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            Configuration = builder.Build();
+        }
+
+        public static string GetConnectionString()
+        {
+            return Configuration.GetConnectionString("LocalConnectionString");
+        }
+    }
+}
---
+++ util/DBPropertyUtil.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace projectmanagement.util
+{
+    internal class DBPropertyUtil
+    {
+        private static IConfigurationRoot Configuration { get; set; }
+
+        static DBPropertyUtil()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory()) // Sets the base path to the current directory
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true); // Adds appsettings.json
+
+            Configuration = builder.Build(); // Build the configuration
+        }
+
+        // Static method to get the connection string
+        public static string GetConnectionString()
+        {
+            return Configuration.GetConnectionString("LocalConnectionString");
+        }
+    }
+}
